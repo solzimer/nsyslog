b4a953cd9e43cba7bc78f91c6c2d83608b1463f5
solzimer <solzimer@gmail.com>
2017-05-09 16:09:42 +0200

    pooled async parser

--- main.js
+++ main.js
@@ -3,11 +3,12 @@ const
 	net = require('net'),
 	program = require("commander"),
 	extend = require("extend"),
-	slparser = require("nsyslog-parser"),
+	slparser = require("./lib/syslog-parser"),
 	configure = require("./lib/config.js"),
 	UDPServer = require("./lib/server/udp.js"),
 	moment = require("moment");
 
+/*
 configure("./config/cfg001.json",(err,cfg)=>{
 	console.log(cfg);
 	var server = new UDPServer();
@@ -20,6 +21,25 @@ configure("./config/cfg001.json",(err,cfg)=>{
 				var tr = cfg.transporters[flow.transporter];
 				tr.send(entry);
 			}
+		});
+	});
+});
+*/
+
+configure("./config/cfg001.json",(err,cfg)=>{
+	console.log(cfg);
+	var server = new UDPServer();
+	server.configure({host:"0.0.0.0",port:514,protocol:"udp4"});
+	server.start(entry=>{
+		slparser(entry,rentry=>{
+			entry = extend(entry,rentry);
+			cfg.flows.forEach(flow=>{
+				var from = flow.from(entry);
+				if(from) {
+					var tr = cfg.transporters[flow.transporter];
+					tr.send(entry);
+				}
+			});
 		});
 	});
 });

414a78f059951ea6627670599d2750f7f5793ed8
solzimer <solzimer@gmail.com>
2017-05-13 00:37:28 +0200

    transporter parallel/serial flow implementation

--- main.js
+++ main.js
@@ -9,48 +9,80 @@ const
 	UDPServer = require("./lib/server/udp.js"),
 	moment = require("moment");
 
-/*
-configure("./config/cfg001.json",(err,cfg)=>{
-	console.log(cfg);
-	var server = new UDPServer();
-	server.configure({host:"0.0.0.0",port:514,protocol:"udp4"});
-	server.start(entry=>{
-		entry = extend(entry,slparser(entry.originalMessage));
-		cfg.flows.forEach(flow=>{
-			var from = flow.from(entry);
-			if(from) {
-				var tr = cfg.transporters[flow.transporter];
-				tr.send(entry);
-			}
-		});
-	});
-});
-*/
-
 var parserStream = new AsyncStream();
 var cfg = null;
+var seq = 0;
 
-parserStream.forEach((err,entry,ex)=>{
-	cfg.flows.forEach(flow=>{
-		var from = flow.from(entry);
-		if(from) {
-			var tr = flow.transporters[0];
-			tr.send(entry);
+function initialize() {
+	configure("./config/cfg001.json",(err,res)=>{
+		cfg = res;
+		console.log(JSON.stringify(cfg));
+		startParserStream();
+		startServers();
+	});
+}
+
+function startParserStream() {
+	parserStream.forEach((err,item,ex)=>{
+		var entry = item.entry;
+		item.flows.
+			filter(flow=>flow.when(entry)).
+			forEach(flow=>transport(entry,flow.transporters));
+	});
+}
+
+function transport(entry,transporters) {
+	return new Promise((resolve,reject)=>{
+		if(transporters.mode=="serial") {
+			var pr = Promise.resolve(entry);
+			transporters.list.forEach(tr=>{
+				if(tr.send) {
+					var npr = new Promise((res,rej)=>{
+						tr.send(entry,()=>res(entry))
+					});
+				}
+				else {
+					var npr = transport(entry,tr);
+				}
+				pr.then(npr);
+				pr = npr;
+			});
+			pr.then(resolve);
+		}
+		else if(transporters.mode=="parallel") {
+			var pr = transporters.list.map(tr=>{
+				if(tr.send) {
+					return new Promise((res,rej)=>{
+						tr.send(entry,()=>res(entry));
+					});
+				}
+				else {
+					return transport(entry,tr);
+				}
+			});
+			Promise.all(pr).then(resolve);
 		}
 	});
-});
+}
 
-configure("./config/cfg001.json",(err,res)=>{
-	console.log(cfg);
-	cfg = res;
-	var seq = 0;
+function collectEntry(entry) {
+	entry.seq = seq++;
+	parserStream.push(new Promise((resolve,reject)=>{
+		var flows = cfg.flows.filter(flow=>flow.from(entry));
+		if(flows.find(flow=>flow.parse)) {
+			slparser(entry,rentry=>resolve({entry:extend(entry,rentry),flows:flows}));
+		}
+		else {
+			resolve({entry:entry,flows:flows});
+		}
+	}));
+}
 
-	var server = new UDPServer();
-	server.configure({host:"0.0.0.0",port:514,protocol:"udp4"});
-	server.start(entry=>{
-		entry.seq = seq++;
-		parserStream.push(new Promise((resolve,reject)=>{
-			slparser(entry,rentry=>resolve(extend(entry,rentry)));
-		}));
-	});
-});
+function startServers() {
+	for(var i in cfg.servers) {
+		var server = cfg.servers[i];
+		server.start(collectEntry);
+	}
+}
+
+initialize();

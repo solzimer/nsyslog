7ef70142c1c94d95ace9cd1dec11ae595f57e8ed
solzimer <solzimer@gmail.com>
2018-06-06 17:21:48 +0200

    fixes

--- test/test001.js
+++ test/test001.js
@@ -4,35 +4,21 @@ const input = new Readable({
 	objectMode : true,
 	read(size) {
 		setTimeout(()=>{
-			if(Math.random()>=0.5) {
-				console.log("It's an error");
-				this.push(null);
-			}
-			else {
-				this.push({seq:"sequence"});
-			}
-		},1000);
+			this.push({seq:"sequence"});
+		},1);
 	}
 });
 
 const output = new Writable({
 	objectMode : true,
   write(chunk, encoding, callback) {
-		console.log("New chunk => ",chunk);
-		callback();
+		setTimeout(()=>{
+			console.log("New chunk => ",chunk);
+			callback();
+		},1000);
   }
 });
 
-input.on('end',()=>{
-	console.log("end")
-	input.emit('readable');
-});
-input.on('close',()=>console.log("close"));
-input.on('readable',()=>console.log("readable"));
-input.on('error',(err)=>{
-	console.error('New Error',err);
-	input.emit('readable');
-});
 input.pipe(output);
 
 setTimeout(()=>{

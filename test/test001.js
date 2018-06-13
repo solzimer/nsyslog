0a32ccd134ad5bad0ea5b35517ec30c6cd626880
solzimer <solzimer@gmail.com>
2018-06-14 00:28:08 +0200

    test

--- test/test001.js
+++ test/test001.js
@@ -1,16 +1,22 @@
 const {Writable, Readable} = require('stream');
 
+var r = 0, w = 0;
+
 const input = new Readable({
 	objectMode : true,
+	highWaterMark : 10000,
 	read(size) {
-		setTimeout(()=>{
-			this.push({seq:"sequence"});
-		},1);
+		r++;
+		console.log(`Called Read ${r}`);
+		setImmediate(()=>{
+			this.push({seq:`sequence ${r}`});
+		});
 	}
 });
 
 const output = new Writable({
 	objectMode : true,
+	highWaterMark : 10000,
   write(chunk, encoding, callback) {
 		setTimeout(()=>{
 			console.log("New chunk => ",chunk);

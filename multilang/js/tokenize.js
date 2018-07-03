5fb08d060275181d50d104106e69100b826b072e
solzimer <solzimer@gmail.com>
2018-07-03 16:08:23 +0200

    multilang tuple getter

---
+++ multilang/js/tokenize.js
@@ -0,0 +1,26 @@
+const {BasicBolt} = require('./storm');
+
+class TokenizeBolt extends BasicBolt {
+	constructor() {
+		super();
+	}
+
+	initialize(conf, context, callback) {
+		this.max = conf.max;
+		callback();
+	}
+
+	process(tup, done) {
+		var words = tup.values[0].split(" ");
+
+		this.emit({tuple: [words.length,words], anchorTupleId: tup.id}, (taskIds)=>{});
+		done();
+	}
+}
+
+if(module.parent) {
+	module.exports = TokenizeBolt;
+}
+else {
+	new TokenizeBolt().run();
+}

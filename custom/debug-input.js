98a397b71588c8ffe5f88103011b19ee3836db6e
David Gomez Matarrodona <solzimer@gmail.com>
2019-01-23 15:26:09 +0100

    kafka tls

---
+++ custom/debug-input.js
@@ -0,0 +1,36 @@
+const Input = require('../lib/input');
+
+class DebugInput extends Input {
+	constructor(id,type) {
+		super(id,type);
+	}
+
+	configure(config,callback) {
+		config = config || {};
+		callback();
+	}
+
+	get mode() {
+		return Input.MODE.pull;
+	}
+
+	start(callback) {
+		this.seq = 0;
+		callback();
+	}
+
+	next(callback) {
+		setTimeout(()=>{
+			callback(null,{
+				"type":"debug",
+				"src_ip" : "0.0.0.0",
+				"dest_ip" : "0.0.0.0",
+				"debug" : {
+					"id" : this.seq++
+				}
+			});
+		},1000);
+	}
+}
+
+module.exports = DebugInput;

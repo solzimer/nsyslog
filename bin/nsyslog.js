eb5340f28b8fecfee4e3806d4fdcdf51fdcb8d4c
David Gómez Matarrodona <dagomez>
2019-12-11 13:17:02 +0100

    Refactor main program

--- bin/nsyslog.js
+++ bin/nsyslog.js
@@ -13,85 +13,111 @@ program.version('0.0.1')
 	.option('-t, --test', 'Only validate config file')
 	.option('-l, --log-file [path]', 'Output log file (default stdout)')
 	.option('-L, --log-level [level]', 'Debug level')
-	.option('-q, --quiet','Disable stats logging')
+	.option('-v, --verbose','Verbose stats logging')
 	.option('--cli', 'Starts CLI session')
 	.option('--cli-start', 'Starts CLI session and flows')
 	.parse(process.argv);
 
-let instance = null;
-logger.setLevel(program.logLevel || 'info');
-if(program.logFile) {
-	logger.setFileTransport({
-		file : path.resolve(program.logFile)
-	});
+function setLogger() {
+	logger.setLevel(program.logLevel || 'info');
+	if(program.logFile) {
+		logger.setFileTransport({
+			file : path.resolve(program.logFile)
+		});
+	}
 }
 
-async function finalize() {
+async function configure() {
+	let path = program.file || `${__dirname}/../data/config.json`;
+	logger.info(`Reading configuration file "${path}"`);
+	let cfg = await Config.read(path,null,program.test);
+
+	// Validation errors
+	if(cfg.$$errors) {
+		cfg.$$errors.forEach(err=>{
+			logger[err.sev||'warn'](err);
+			if(err.err) logger.error(err.err);
+		});
+		if(cfg.$$errors.filter(err=>err.sev=='error').length)	{
+			logger.error(`Config file has severe errors. Cannot continue`);
+			throw new Error('Configuration severe errors');
+		}
+	}
+	else {
+		logger.info('Valid config file');
+	}
+
+	return cfg;
+}
+
+function createInstance(cfg) {
+	let nsyslog = new NSyslog(cfg);
+	let instance = {path,stats,nsyslog};
+
+	nsyslog.on('stats',other=>{
+		stats.merge(other);
+	});
+
+	nsyslog.on('destroy',nsyslog=>{
+		nsyslog.removeAllListeners();
+	});
+
+	return instance;
+}
+
+async function startCLI(instance) {
 	let nsyslog = instance.nsyslog;
-	try {await nsyslog.stop();}catch(err){logger.error(err);}
-	setTimeout(()=>process.exit(1),500);
+
+	if(program.cliStart) {
+		await nsyslog.start();
+	}
+
+	require('../lib/cli')(instance,'nsyslog');
+	process.on('SIGINT',()=>{});
+	process.on('SIGTERM', ()=>{});
+}
+
+async function startStandalone(instance) {
+	let nsyslog = instance.nsyslog;
+
+	async function finalize() {
+		try {await nsyslog.stop();}catch(err){logger.error(err);}
+		setTimeout(()=>process.exit(1),500);
+	}
+
+	process.on('SIGTERM', finalize);
+	process.on('SIGINT', finalize);
+
+	if(program.verbose) {
+		setInterval(()=>{
+			logger.info(`${new Date()} : ${JSON.stringify(stats.all())}`);
+		},1000);
+	}
+
+	await nsyslog.start();
 }
 
 async function initialize() {
 	try {
 		// Read configuration
-		logger.warn('NSyslog is on beta status, not ready for production yet!');
-		let path = program.file || `${__dirname}/../data/config.json`;
-		logger.info(`Reading configuration file "${path}"`);
-		let cfg = await Config.read(path,null,program.test);
-
-		// Validation errors
-		if(cfg.$$errors) {
-			cfg.$$errors.forEach(err=>{
-				logger[err.sev||'warn'](err);
-				if(err.err) logger.error(err.err);
-			});
-			if(cfg.$$errors.filter(err=>err.sev=='error').length)	{
-				logger.error(`Config file has severe errors. Cannot continue`);
-				process.exit(1);
-			}
-		}
-		else {
-			logger.info('Valid config file');
-		}
+		setLogger();
+		logger.info(`Welcome to NSyslog`);
+		let cfg = await configure();
 
 		// Only test config. Exit
 		if(program.test) {
 			process.exit(0);
 		}
 
+		// Create NSyslog instance
 		logger.info(`Config loaded!`);
-		let nsyslog = new NSyslog(cfg);
-		instance = {path,stats,nsyslog};
-
-		nsyslog.on('stats',other=>{
-			stats.merge(other);
-		});
-
-		nsyslog.on('destroy',nsyslog=>{
-			nsyslog.removeAllListeners();
-		});
+		let instance = createInstance(cfg);
 
 		if(program.cli || program.cliStart) {
-			if(program.cliStart) {
-				await nsyslog.start();
-			}
-
-			require('../lib/cli')(instance,'nsyslog');
-			process.on('SIGINT',()=>{});
-			process.on('SIGTERM', ()=>{});
+			startCLI(instance);
 		}
 		else {
-			process.on('SIGTERM', finalize);
-			process.on('SIGINT', finalize);
-
-			if(!program.quiet) {
-				setInterval(()=>{
-					logger.debug(`${new Date()} : ${JSON.stringify(stats.all())}`);
-				},1000);
-			}
-
-			await nsyslog.start();
+			startStandalone(instance);
 		}
 	}catch(err) {
 		logger.error(err);
